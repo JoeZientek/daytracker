cada603b73b1e17ad8744efc73b90fd4c1177a22
JoeZientek <jtzientek@gmail.com>
2024-01-04 12:17:44 -0500

    created basic package infrastructure and added polished auth

---
+++ inst/global.R
@@ -0,0 +1,5 @@
+# configuration for polished auth
+polished::polished_config(
+  app_name = "daytracker",
+  api_key = Sys.getenv("polished_key")
+)
---
+++ inst/server.R
@@ -0,0 +1,12 @@
+server <- function(input, output, session) {
+  output$user_out <- renderPrint({
+    session$userData$user()
+  })
+  
+  observeEvent(input$sign_out, {
+    polished::sign_out_from_shiny()
+    session$reload()
+  })
+}
+
+polished::secure_server(server)
